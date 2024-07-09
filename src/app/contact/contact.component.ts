3f66898dc1a79c0eeda0f4be75614208639a089b
Claire Vannier <clairevannier@MacBook-Pro-de-Claire.local>
2024-07-09 15:48:55 +0200

    finish integer email.js to send an email from front-end

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    emailjs.init(environment.emailjs.publicKey);
+  }
+
+  sendEmail() {
+    if (this.contactForm.valid) {
+      const form = this.contactForm.value;
+      emailjs.send(environment.emailjs.serviceID, environment.emailjs.templateID, form)
+        .then((result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          alert('Message envoyé avec succès!');
+        }, (error) => {
+          console.log(error.text);
+          alert('Échec de l\'envoi du message.');
+        });
+    } else {
+      alert('Veuillez remplir tous les champs correctement.');
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  emailjs: {
+    publicKey: 'qhlWHk1VPtRPNhbql',
+    serviceID: 'service_t2jqftf',
+    templateID: 'template_vaxcxkw'
+  }
+};
